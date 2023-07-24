fc8ada2e542cc87616eb1f067912b41bb7b65e77
f11 <eieioeiji0501@gmail.com>
2023-07-24 19:05:48 +0900

    [update]update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,31 @@
-#!/bin/sh
-
+#!/bin/bash
+RUN_NAME="ros_noetic:latest"
 xhost +local:user
-docker run -it \
-  --runtime=nvidia \
-  --rm \
-  --env="DISPLAY" \
-  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-  --env="QT_X11_NO_MITSHM=1" \
-  --net host \
-  --privileged \
-  -v "/etc/localtime:/etc/localtime:ro" \
-  ros_noetic:latest \
-  bash
+if [ $# -eq 1 ]; then
+  if [ $1 = "-g" ]; then
+    echo "GPU available";
+    docker run -it \
+      --runtime=nvidia \
+      --rm \
+      --env="DISPLAY" \
+      --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+      --env="QT_X11_NO_MITSHM=1" \
+      --net host \
+      --privileged \
+      -v "/etc/localtime:/etc/localtime:ro" \
+      $RUN_NAME \
+      bash
+  fi
+else
+  echo "GPU Disabled";
+  docker run -it \
+    --rm \
+    --env="DISPLAY" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --net host \
+    --privileged \
+    -v "/etc/localtime:/etc/localtime:ro" \
+    $RUN_NAME \
+    bash
+fi
